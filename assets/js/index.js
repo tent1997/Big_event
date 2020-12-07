784a098f119a1b5c5bab260b7d587d16c607e54d
zp <1974490292@qq.com>
2020-12-07 13:04:13 +0800

    12-07 个人信息已完成

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function () {
+    getUserInfo()
+    $('#loginout').click(function () {
+        var layer = layui.layer
+        console.log(layer);
+        layer.confirm('确定要退出吗?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            localStorage.removeItem('token')
+            location.href = './login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    // $('.text-avatar')[0].show()  
+    // $('.layui-nav-img').hide()
+    var uname = user.nickname || user.username
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        $('.text-avatar').show()
+        $('.text-avatar').html(uname[0].toUpperCase())
+    }
+    $('.first').html('欢迎&nbsp;&nbsp;' + uname)
+}
---
+++ assets/js/login.js
@@ -0,0 +1,60 @@
+$(function () {
+    $('#regbtn').click(function (e) {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#loginbtn').click(function (e) {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    var form = layui.form
+    var layer = layui.layer;
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            if (value !== $('#reg-form [name=password]').val()) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+    $('#reg-form').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            data: {
+                username: $('#uname').val(),
+                password: $('#pwd').val()
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg(res.message)
+                $('#loginbtn').click()
+            }
+        })
+    })
+    $('#login-form').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: {
+                username: $('#loguname').val(),
+                password: $('#logpwd').val()
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg(res.message)
+                localStorage.setItem('token', res.token)
+                // location.href = '/index.html'
+                location.href = './index.html'
+            }
+        })
+    })
+})
